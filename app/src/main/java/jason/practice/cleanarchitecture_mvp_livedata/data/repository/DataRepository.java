1b813422c819492aa208c9b875d392746dc2f5d0
Jason1 Su (蘇暐凱_Askey_TW) <jason1_su@askey.com>
2021-06-19 11:18:11 +0800

    comply dependency inversion principle

--- app/src/main/java/jason/practice/cleanarchitecture_mvp_livedata/data/repository/DataRepository.java
+++ app/src/main/java/jason/practice/cleanarchitecture_mvp_livedata/data/repository/DataRepository.java
@@ -1,25 +1,15 @@
 package jason.practice.cleanarchitecture_mvp_livedata.data.repository;
 
-import androidx.lifecycle.MutableLiveData;
-
-import jason.practice.cleanarchitecture_mvp_livedata.data.remote.MobileApiImp;
+import jason.practice.cleanarchitecture_mvp_livedata.data.remote.MobileApi;
 import jason.practice.cleanarchitecture_mvp_livedata.data.remote.Response;
 import jason.practice.cleanarchitecture_mvp_livedata.model.WifiModel;
 
 public class DataRepository {
 
-    private final MobileApiImp api;
+    private final MobileApi api;
 
-    public DataRepository(MobileApiImp api) {
+    public DataRepository(MobileApi api) {
         this.api = api;
-    }
-
-    public void fetchWifiData(MutableLiveData<WifiModel> wifi2GMode, MutableLiveData<WifiModel> wifi5GModel) throws Throwable {
-        Response res1 = api.getWifiData(1);
-        wifi2GMode.postValue(new WifiModel("Jason-Wifi_2G", "12345678", 3, 1));
-
-        Response res2 = api.getWifiData(0);
-        wifi5GModel.postValue(new WifiModel("Jason-Wifi_5G", "12345678", 3, 0));
     }
 
     public WifiModel fetchWifiData(int _interface) throws Throwable {
